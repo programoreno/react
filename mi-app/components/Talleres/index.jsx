6036a026f8a77094e22e4e5f5457518bfbcc7ced
Guille <97970403+programoreno@users.noreply.github.com>
2026-06-06 12:33:54 +0200

    Hovers en cards y arreglos en el class

--- mi-app/components/Talleres/index.jsx
+++ mi-app/components/Talleres/index.jsx
@@ -3,9 +3,9 @@ import "./Talleres.css"
 export default function Talleres(){
     return(
         <section>
-            <div class="cards">Terapia Online</div>
-            <div class="cards">Terapia a Domicilio</div>
-            <div class="cards">Estimulación Cognitiva</div>
+            <div className="cards">Terapia Online</div>
+            <div className="cards">Terapia a Domicilio</div>
+            <div className="cards">Estimulación Cognitiva</div>
         </section>
     );
 }
