5ce1efcef96f1a1809e85a0db0f028abe46a6c96
Guille <97970403+programoreno@users.noreply.github.com>
2026-06-06 17:07:45 +0200

    Mod footer y metidos más divs

--- mi-app/components/Footer/index.jsx
+++ mi-app/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import Button from "@/components/ui/Buttons";
 import "./Footer.css";
+import Link from 'next/link';
 
 export default function Footer() {
     return (
@@ -7,6 +8,9 @@ export default function Footer() {
             Footer
 
             <Button>Saber más</Button>
+            <Link href="/">Inicio</Link>
+            <Link href="/home">Home</Link>
+            <Link href="/about">About</Link>
             {/* <Button type="submit">Enviar formulario</Button> */}
         </section>
     );
--- mi-app/components/Talleres/index.jsx
+++ mi-app/components/Talleres/index.jsx
@@ -2,10 +2,18 @@ import "./Talleres.css"
 
 export default function Talleres(){
     return(
-        <section>
-            <div className="cards">Terapia Online</div>
-            <div className="cards">Terapia a Domicilio</div>
-            <div className="cards">Estimulación Cognitiva</div>
-        </section>
+        <article>
+            <section>
+                <div className="cards">Terapia Online</div>
+                <div className="cards">Terapia a Domicilio</div>
+                <div className="cards">Estimulación Cognitiva</div>
+            </section>
+
+            <section>
+                <div className="cards">Terapia Online</div>
+                <div className="cards">Terapia a Domicilio</div>
+                <div className="cards">Estimulación Cognitiva</div>
+            </section>
+        </article>
     );
 }
